c56afc75bb7b4cad021b7ed25a0d2c98ddf553fb
andresTs <andres91411@gmail.com>
2024-02-02 22:37:57 -0600

    changes

--- src/app/core/models/wallet-model-1b/balance-information-1b.model.ts
+++ src/app/core/models/wallet-model-1b/balance-information-1b.model.ts
@@ -3,11 +3,13 @@ export class BalanceInformationModel1B {
   totalAcquisitions: number;
   availableBalance: number;
   totalCommissionsPaid: number;
+  serviceBalance: number;
 
   constructor() {
     this.reverseBalance = 0;
     this.totalAcquisitions = 0;
     this.availableBalance = 0;
     this.totalCommissionsPaid = 0;
+    this.serviceBalance = 0;
   }
 }
