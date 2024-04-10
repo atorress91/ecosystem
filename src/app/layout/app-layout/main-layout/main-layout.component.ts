11cb15e0675840dc508facdb2cbb63383bb80e2d
andresTs <andres91411@gmail.com>
2024-04-10 16:42:52 -0600

    changes

--- src/app/layout/app-layout/main-layout/main-layout.component.ts
+++ src/app/layout/app-layout/main-layout/main-layout.component.ts
@@ -26,9 +26,9 @@ export class MainLayoutComponent implements OnInit {
 
   ngOnInit() {
     this.user = this.authService.currentUserAffiliateValue;
-    // if (this.user.message_alert == 0) {
-    //   this.showAlert();
-    // }
+    if (this.user.message_alert == 0) {
+      this.showAlert();
+    }
   }
 
   ngAfterViewInit(): void {
@@ -61,24 +61,25 @@ export class MainLayoutComponent implements OnInit {
     })
   }
 
-  // showAlert() {
-  //   Swal.fire({
-  //     icon: "info",
-  //     title: 'Celebración Especial de Ecosystem',
-  //     html: `
-  //           <p>Querida familia de Ecosystem,</p>
-  //           <p>En este día tan especial, queremos celebrar la maravilla de conectar, compartir y crecer juntos. Que este día sea un recordatorio de que la verdadera riqueza se encuentra en los lazos que tejemos.</p>
-  //           <p>Brindemos por el amor, la amistad y el éxito compartido!</p>
-  //       `,
-  //     confirmButtonText: 'OK',
-  //     confirmButtonColor: '#3085d6',
-  //     showCancelButton: false,
-  //   }).then((result) => {
-  //     if (result.isConfirmed) {
-  //       this.messageReceived();
-  //     }
-  //   });
-  // }
+  showAlert() {
+    Swal.fire({
+      icon: "info",
+      title: 'Habilitación de Retiros de Saldo disponible a su billetera',
+      html: `
+            <p>Querida familia de Ecosystem,</p>
+            <p>Nos complace anunciar que el próximo martes, 23 de abril, estaremos habilitando los retiros de saldo. Esta es una oportunidad para que todos nuestros miembros puedan gestionar sus recursos de manera más efectiva dentro de nuestra plataforma.</p>
+            <p>¡Agradecemos su paciencia y confianza en nosotros! Prepárense para realizar sus retiros.</p>
+        `,
+      confirmButtonText: 'OK',
+      confirmButtonColor: '#3085d6',
+      showCancelButton: false,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.messageReceived();
+      }
+    });
+  }
+
 
   showSuccess(message: string) {
     this.toast.success(message);
