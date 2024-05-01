2a860f6cd0ec3a82bb30cf836659145aa9bb564d
jeffdel199414 <jedelsan199414@gmail.com>
2024-05-01 10:15:16 -0600

    Add statistics data retrieval for network details
    A new function `getStatisticsInformationByAffiliateId` is added in `WalletService` to provide detailed statistics information related to each user affiliate. These include amounts for different models, volumes, and user counts. The network details component and its corresponding HTML have been updated to display these statistics. The environmental variables and tokens were also updated.

---
+++ src/app/core/models/wallet-model/statisticsInformation.ts
@@ -0,0 +1,26 @@
+export class StatisticsInformation
+{
+  amountPoolModel1A: number;
+  amountPoolModel1B: number;
+  AmountPoolModel2: number;
+  AmountPoolModel3: number;
+  VolumeLeftModel4: number;
+  VolumeRightModel4: number;
+  AmountChildrenLeftModel4: number;
+  AmountChildrenRightModel4: number;
+  AmountPassiveModel1A: number;
+  AmountResidualModel1A: number;
+  AmountPassiveModel1B: number;
+  AmountResidualModel1B: number;
+  AmountPassiveModel2: number;
+  AmountResidualModel2: number;
+  AmountPercentageModel3: number;
+  AmountResidualModel3: number;
+  AmountUsersDirectModel123: number;
+  AmountUsersNetworkModel123: number;
+  AmountUsersDirectModel5: number;
+  AmountUsersNetworkModel5: number;
+  AmountUsersDirectModel6: number;
+  AmountUsersNetworkModel6: number;
+
+}
